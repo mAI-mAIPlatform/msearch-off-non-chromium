bc40177eef06fa45277338571204f14f0deabeb2
mDevlopers <mathias.tss2012@gmail.com>
2026-02-06 15:30:09 +0100

    Update tabsManager.js

--- features/tabs/tabsManager.js
+++ features/tabs/tabsManager.js
@@ -1,16 +1,54 @@
-let tabs = [];
+const { BrowserView } = require("electron");
 
-function create(url) {
+let tabs = [];
+let currentTab = null;
+
+function createTab(win, url) {
+  const view = new BrowserView({
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false
+    }
+  });
+
+  win.setBrowserView(view);
+
+  view.setBounds({
+    x: 320,      // largeur sidebar mAI
+    y: 64,       // hauteur topbar
+    width: win.getBounds().width - 320,
+    height: win.getBounds().height - 64
+  });
+
+  view.setAutoResize({ width: true, height: true });
+  view.webContents.loadURL(url);
+
   const tab = {
     id: Date.now(),
+    view,
     url
   };
+
   tabs.push(tab);
+  currentTab = tab;
+
   return tab;
 }
 
-function list() {
+function switchTab(win, id) {
+  const tab = tabs.find(t => t.id === id);
+  if (!tab) return;
+
+  win.setBrowserView(tab.view);
+  currentTab = tab;
+}
+
+function getTabs() {
   return tabs;
 }
 
-module.exports = { create, list };
+module.exports = {
+  createTab,
+  switchTab,
+  getTabs
+};
