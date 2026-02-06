4b3d9e6532204ca7ce38c6fc107cca8a13d05b50
mDevlopers <mathias.tss2012@gmail.com>
2026-02-06 12:10:29 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+
+let win;
 
 function createWindow() {
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 1280,
     height: 800,
-    webPreferences: { preload: `${__dirname}/preload.js` }
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: false,
+      contextIsolation: true
+    }
   });
+
   win.loadFile('renderer/index.html');
 }
 
 app.whenReady().then(createWindow);
+
+// Navigation depuis la barre de recherche
+ipcMain.on('navigate', (event, url) => {
+  if (!url.startsWith('http')) url = 'https://' + url;
+  win.loadURL(url);
+});
