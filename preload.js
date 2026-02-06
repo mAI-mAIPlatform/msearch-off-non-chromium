8f992c5a44df901e9713d8f85f91fdf8a4252a49
mDevlopers <mathias.tss2012@gmail.com>
2026-02-06 17:58:36 +0100

    Update preload.js

--- preload.js
+++ preload.js
@@ -3,5 +3,11 @@ const { contextBridge, ipcRenderer } = require("electron");
 contextBridge.exposeInMainWorld("electronAPI", {
   navigate: (url) => ipcRenderer.send("navigate", url),
   newTab: (url) => ipcRenderer.send("new-tab", url),
-  bookmark: (data) => ipcRenderer.send("bookmark", data)
+  bookmark: (data) => ipcRenderer.send("bookmark", data),
+  addProfile: (name) => ipcRenderer.send("add-profile", name),
+  newDownload: (file) => ipcRenderer.send("new-download", file),
+  addReminder: (data) => ipcRenderer.send("add-reminder", data),
+  addNote: (data) => ipcRenderer.send("add-note", data),
+  addCategory: (name) => ipcRenderer.send("add-category", name),
+  newTabGroup: (name) => ipcRenderer.send("new-tab-group", name)
 });
