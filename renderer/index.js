c8f969827858c0f83fca2388a033e77b1e522db4
mDevlopers <mathias.tss2012@gmail.com>
2026-02-06 15:47:36 +0100

    Update index.js

--- renderer/index.js
+++ renderer/index.js
@@ -1,5 +1,7 @@
 const input = document.getElementById("search");
 const goBtn = document.getElementById("goBtn");
+const newTabBtn = document.getElementById("newTabBtn");
+const favBtn = document.getElementById("favBtn");
 
 function normalize(value) {
   if (!value.startsWith("http")) {
@@ -9,25 +11,26 @@ function normalize(value) {
 }
 
 function navigate() {
-  const url = normalize(input.value.trim());
+  const url = normalize(input.value);
   window.electronAPI.navigate(url);
 }
 
 function newTab() {
-  const url = normalize(input.value.trim() || "google.com");
+  const url = normalize(input.value || "google.com");
   window.electronAPI.newTab(url);
 }
 
-input.addEventListener("keydown", (e) => {
+function bookmark() {
+  window.electronAPI.bookmark({
+    title: input.value,
+    url: normalize(input.value)
+  });
+}
+
+goBtn.onclick = navigate;
+newTabBtn.onclick = newTab;
+favBtn.onclick = bookmark;
+
+input.addEventListener("keydown", e => {
   if (e.key === "Enter") navigate();
-});
-
-goBtn.addEventListener("click", navigate);
-
-// Ctrl + T = nouvel onglet
-document.addEventListener("keydown", (e) => {
-  if (e.ctrlKey && e.key === "t") {
-    e.preventDefault();
-    newTab();
-  }
 });
