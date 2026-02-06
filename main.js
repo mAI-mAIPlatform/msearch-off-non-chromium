7e7dabe108323872e59df8d9a598872840c5a73d
mDevlopers <mathias.tss2012@gmail.com>
2026-02-06 15:32:41 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,68 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
-const path = require('path');
+const { app, BrowserWindow, BrowserView, ipcMain } = require("electron");
+const path = require("path");
 
 let win;
+let tabs = [];
+let currentTab = null;
 
-function createWindow() {
-  win = new BrowserWindow({
-    width: 1280,
-    height: 800,
+function createTab(url) {
+  const view = new BrowserView({
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-      nodeIntegration: false,
-      contextIsolation: true
+      contextIsolation: true,
+      nodeIntegration: false
     }
   });
 
-  win.loadFile('renderer/index.html');
+  win.setBrowserView(view);
+
+  const bounds = win.getBounds();
+
+  view.setBounds({
+    x: 320,      // sidebar mAI
+    y: 64,       // topbar
+    width: bounds.width - 320,
+    height: bounds.height - 64
+  });
+
+  view.setAutoResize({ width: true, height: true });
+  view.webContents.loadURL(url);
+
+  const tab = {
+    id: Date.now(),
+    view,
+    url
+  };
+
+  tabs.push(tab);
+  currentTab = tab;
 }
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    backgroundColor: "#0a0a0a",
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js")
+    }
+  });
+
+  win.loadFile("renderer/index.html");
+
+  // Onglet par défaut
+  createTab("https://www.google.com");
+}
+
+ipcMain.on("navigate", (_, url) => {
+  if (currentTab) currentTab.view.webContents.loadURL(url);
+});
+
+ipcMain.on("new-tab", (_, url) => {
+  createTab(url);
+});
 
 app.whenReady().then(createWindow);
 
-// Navigation depuis la barre de recherche
-ipcMain.on('navigate', (event, url) => {
-  if (!url.startsWith('http')) url = 'https://' + url;
-  win.loadURL(url);
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
 });
